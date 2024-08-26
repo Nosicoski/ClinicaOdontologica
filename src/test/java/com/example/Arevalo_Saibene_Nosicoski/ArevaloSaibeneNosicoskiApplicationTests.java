fc4841d39b94e437fb508d2bca6d2d93b23da582
Juanma Nosicoski <juanmanosicoski@hotmail.com>
2024-08-26 17:19:02 -0300

    commit1

---
+++ src/test/java/com/example/Arevalo_Saibene_Nosicoski/ArevaloSaibeneNosicoskiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Arevalo_Saibene_Nosicoski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArevaloSaibeneNosicoskiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
