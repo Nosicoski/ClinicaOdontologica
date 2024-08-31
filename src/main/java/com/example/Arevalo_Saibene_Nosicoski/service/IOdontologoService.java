026009e83b72de7f4d43f099d3695997a6bff17a
@Ignacioaj131982 <ignacioarevalo@gmail.com>
2024-08-31 12:55:47 -0300

    Implementacion de OndontologoService completada y IOdontologoService completada

--- src/main/java/com/example/Arevalo_Saibene_Nosicoski/service/IOdontologoService.java
+++ src/main/java/com/example/Arevalo_Saibene_Nosicoski/service/IOdontologoService.java
@@ -2,9 +2,17 @@ package com.example.Arevalo_Saibene_Nosicoski.service;
 
 import com.example.Arevalo_Saibene_Nosicoski.model.Odontologo;
 
+import java.util.List;
+
 import java.util.Optional;
 
 public interface IOdontologoService {
     Odontologo guardarOdontologo(Odontologo odontologo);
     Optional<Odontologo> buscarPorId(Integer id);
+    void eliminarOdontologo(Integer id);
+    List<Odontologo> listarOdontologos();
+
+
 }
+
+
--- src/main/java/com/example/Arevalo_Saibene_Nosicoski/service/impl/OdontologoService.java
+++ src/main/java/com/example/Arevalo_Saibene_Nosicoski/service/impl/OdontologoService.java
@@ -1,29 +1,48 @@
 package com.example.Arevalo_Saibene_Nosicoski.service.impl;
-
+import java.util.List;
 import com.example.Arevalo_Saibene_Nosicoski.model.Odontologo;
 import com.example.Arevalo_Saibene_Nosicoski.repository.IOdontologoRepository;
 import com.example.Arevalo_Saibene_Nosicoski.service.IOdontologoService;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class OdontologoService  {
-    private IOdontologoRepository iOdontologoRepository;
+public class OdontologoService implements IOdontologoService  {
+    private final IOdontologoRepository iOdontologoRepository;
 
     public OdontologoService(IOdontologoRepository iOdontologoRepository) {
         this.iOdontologoRepository = iOdontologoRepository;
 
     }
 
+    @Transactional
+    @Override
     public Odontologo guardarOdontologo(Odontologo odontologo) {
         return iOdontologoRepository.save(odontologo);
     }
 
-
-    public Optional<Odontologo> buscarPorId(Integer id) {
+    @Override
+public Optional<Odontologo> buscarPorId(Integer id) {
         return iOdontologoRepository.findById(id);
     }
+
+    @Transactional
+    @Override
+    public void eliminarOdontologo(Integer id) {
+        iOdontologoRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Odontologo> listarOdontologos() {
+        return iOdontologoRepository.findAll();
+    }
 }
+
+
+
+
+
 
 
