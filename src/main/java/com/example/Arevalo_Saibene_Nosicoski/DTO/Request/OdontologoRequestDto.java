6cb9f4effce08a440ce25d3a689644e6c3192951
@Ignacioaj131982 <ignacioarevalo@gmail.com>
2024-08-31 11:27:49 -0300

    trabajando odontologo request

--- src/main/java/com/example/Arevalo_Saibene_Nosicoski/DTO/Request/OdontologoRequestDto.java
+++ src/main/java/com/example/Arevalo_Saibene_Nosicoski/DTO/Request/OdontologoRequestDto.java
@@ -1,4 +1,19 @@
 package com.example.Arevalo_Saibene_Nosicoski.DTO.Request;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class OdontologoRequestDto {
+    @NotBlank(message = "Número de matrícula no puede estar vacío")
+    private String nroMatricula;
+    private String apellido;
+    private String nombre;
 }
