c8992f076275eb94322fea847bb64711dfdbd08b
@Ignacioaj131982 <ignacioarevalo@gmail.com>
2024-09-09 15:51:20 -0300

    Eliminado Domicilio Controlador Service e Impl

--- src/main/java/com/example/Arevalo_Saibene_Nosicoski/Config/ModelMapperConfig.java
+++ src/main/java/com/example/Arevalo_Saibene_Nosicoski/Config/ModelMapperConfig.java
@@ -1,16 +1,7 @@
 package com.example.Arevalo_Saibene_Nosicoski.Config;
 
-import com.example.Arevalo_Saibene_Nosicoski.DTO.Response.PacienteResponseDto;
-import com.example.Arevalo_Saibene_Nosicoski.DTO.Response.TurnoResponseDto;
-import com.example.Arevalo_Saibene_Nosicoski.DTO.Response.OdontologoResponseDto;
-import com.example.Arevalo_Saibene_Nosicoski.DTO.Response.DomicilioResponseDto;
-import com.example.Arevalo_Saibene_Nosicoski.model.Domicilio;
-import com.example.Arevalo_Saibene_Nosicoski.model.Paciente;
-import com.example.Arevalo_Saibene_Nosicoski.model.Turno;
-import com.example.Arevalo_Saibene_Nosicoski.model.Odontologo;
 import jakarta.annotation.PostConstruct;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,62 +9,9 @@ import org.springframework.context.annotation.Configuration;
 public class ModelMapperConfig {
 
     @Bean
-    public ModelMapper modelMapper() {
-        ModelMapper modelMapper = new ModelMapper();
-
-        // Configuración de mapeo para Odontologo a OdontologoResponseDto
-        modelMapper.addMappings(new PropertyMap<Odontologo, OdontologoResponseDto>() {
-            @Override
-            protected void configure() {
-                map().setId(source.getId());
-                map().setNroMatricula(source.getNroMatricula());
-                map().setApellido(source.getApellido());
-                map().setNombre(source.getNombre());
-            }
-        });
-
-        // Mapeo de Domicilio a DomicilioResponseDto
-        modelMapper.addMappings(new PropertyMap<Domicilio, DomicilioResponseDto>() {
-            @Override
-            protected void configure() {
-                map().setId(source.getId());
-                map().setCalle(source.getCalle());
-                map().setNumero(source.getNumero());
-                map().setLocalidad(source.getLocalidad());
-                map().setProvincia(source.getProvincia());
-            }
-        });
-
-        // Configuración de mapeo para Paciente a PacienteResponseDto
-        modelMapper.addMappings(new PropertyMap<Paciente, PacienteResponseDto>() {
-            @Override
-            protected void configure() {
-                map().setId(source.getId());
-                map().setNombre(source.getNombre());
-                map().setApellido(source.getApellido());
-                map().setDni(source.getDni());
-                map().setFechaIngreso(source.getFechaIngreso());
-                map().setDomicilioResponseDto(source.getDomicilio() != null ?
-                        modelMapper.map(source.getDomicilio(), DomicilioResponseDto.class) : null);
-            }
-        });
-
-        // Configuración de mapeo para Turno a TurnoResponseDto
-        modelMapper.addMappings(new PropertyMap<Turno, TurnoResponseDto>() {
-            @Override
-            protected void configure() {
-                map().setId(source.getId());
-                map().setFecha(source.getFecha().toString());
-                map().setPacienteResponseDto(source.getPaciente() != null ?
-                        modelMapper.map(source.getPaciente(), PacienteResponseDto.class) : null);
-                map().setOdontologoResponseDto(source.getOdontologo() != null ?
-                        modelMapper.map(source.getOdontologo(), OdontologoResponseDto.class) : null);
-            }
-        });
-
-        return modelMapper;
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
-
     @PostConstruct
     public void init() {
         System.out.println("ModelMapper bean created!");
